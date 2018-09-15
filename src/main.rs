3e268993bcd50a811ce7f0aafc431b1a9e1aaca0
h3nnn4n <uber.renan@gmail.com>
2018-09-15 13:27:54 -0300

    Added a basic rendered

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate png;
+
 fn main() {
-    println!("Hello, world!");
+    let outfile_name = String::from("out.png");
+
+    let width:u32 = 800;
+    let height:u32 = 600;
+
+    let mut bytes:Vec<u8> = Vec::new();
+
+    for i in 0..width {
+        for j in 0..height {
+            for _ in 0..4 {
+                bytes.push((i + j) as u8);
+            }
+        }
+    }
+
+    write_bytes(&outfile_name, &bytes, width, height);
+}
+
+fn write_bytes(outfile_name: &String, buf: &Vec<u8>, width: u32, height: u32) -> () {
+    use png::HasParameters;
+    let path = std::path::Path::new(outfile_name);
+    let outfile = std::fs::File::create(path).unwrap();
+    let ref mut wr = std::io::BufWriter::new(outfile);
+    let mut encoder = png::Encoder::new(wr, width, height);
+    encoder.set(png::ColorType::RGBA).set(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+    writer.write_image_data(&buf).unwrap();
 }
