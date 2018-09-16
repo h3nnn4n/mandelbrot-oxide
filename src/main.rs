89abba1c5888ffede2d7b33abd0cc0758e689d68
h3nnn4n <uber.renan@gmail.com>
2018-09-16 19:21:32 -0300

    WIP on mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
+extern crate num;
 extern crate png;
+use num::complex::Complex;
 
 fn main() {
     let outfile_name = String::from("out.png");
 
-    let width:u32 = 800;
-    let height:u32 = 600;
+    let width: u32 = 800;
+    let height: u32 = 600;
 
-    let mut bytes:Vec<u8> = Vec::new();
+    let mut bytes: Vec<u8> = Vec::new();
 
     for i in 0..width {
         for j in 0..height {
-            for _ in 0..4 {
-                bytes.push((i + j) as u8);
+            let c = get_c(i, j, width, height);
+            let v = mandelbrot(c);
+
+            for _ in 0..3 {
+                bytes.push(v);
             }
+            bytes.push(255);
         }
     }
 
     write_bytes(&outfile_name, &bytes, width, height);
+}
+
+fn get_c(x: u32, y: u32, width: u32, height: u32) -> (Complex<f64>) {
+    let xcenter = -0.650;
+    let ycenter = 0.0;
+
+    let zoom = -1.500;
+
+    let minx = xcenter + zoom;
+    let maxx = xcenter - zoom;
+    let miny = ycenter + zoom;
+    let maxy = ycenter - zoom;
+
+    let real = minx + (x as f64) * (maxx - minx) / (width as f64);
+    let imag = miny + (y as f64) * (maxy - miny) / (height as f64);
+
+    let c = Complex { re: real, im: imag };
+
+    c
+}
+
+fn mandelbrot(c: Complex<f64>) -> (u8) {
+    let r = (c.norm() * 255.0) as u64;
+
+    (r % 255) as u8
 }
 
 fn write_bytes(outfile_name: &String, buf: &Vec<u8>, width: u32, height: u32) -> () {
